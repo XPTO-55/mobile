a93fb54114b4628f905ef17c3ca93ba21e277744
Leojonass <leo.serafim@bandtec.com.br>
2023-05-17 19:36:26 -0300

    Telas historico do profissional, home e sobre profissional

---
+++ app/src/main/java/com/example/cpaweb/CardQueryHistoric.kt
@@ -0,0 +1,11 @@
+package com.example.cpaweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardQueryHistoric : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_query_historic)
+    }
+}
